c5b4b7b9d359f8c739e1a37da9fc06c155fda3e6
JDLuke <jd@jdluke.com>
2014-11-15 08:19:17 -0500

    Initial commit

---
+++ src/main/java/com/oopuniversity/basics/functions/Add.java
@@ -0,0 +1,15 @@
+package com.oopuniversity.basics.functions;
+
+/**
+ * Created by jluke on 11/15/2014.
+ */
+public class Add {
+    public static void main(String[] args) {
+        int answer = add ( 2 , 3 );
+        System.out.println("The answer is " + answer );
+    }
+
+    public static int add ( int number1, int number2 ) {
+        return number1 + number2;
+    }
+}
---
+++ src/main/java/com/oopuniversity/basics/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.oopuniversity.basics.helloworld;
+
+/**
+ * Created by jluke on 11/15/2014.
+ */
+public class HelloWorld {
+    public static void main(String [] args) {
+        System.out.println("Hello, world!");
+    }
+}
